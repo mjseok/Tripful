707ac2a174fae4d22de7e667ae98c499fe3ba84d
mjseok <mjseok98@gmail.com>
2022-11-16 04:58:19 +0900

    [BE]: [FIX] #1 SpotController Restful하게 수정 spotcontroller파일 restful하게 수정

--- BE/src/main/java/com/ssafy/enjoytrip/spot/controller/SpotController.java
+++ BE/src/main/java/com/ssafy/enjoytrip/spot/controller/SpotController.java
@@ -1,16 +1,16 @@
 package com.ssafy.enjoytrip.spot.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.ssafy.enjoytrip.social.Review;
 import com.ssafy.enjoytrip.spot.Spot;
 import com.ssafy.enjoytrip.spot.service.SpotService;
 
@@ -20,23 +20,25 @@ public class SpotController {
 	
 	@Autowired
 	SpotService spotService;
+	private static final String SUCCESS = "success";
+	private static final String FAIL = "fail";
 	
-	@GetMapping("/mapSearch")
-	public ModelAndView mapSearch() {
-		ModelAndView mav = new ModelAndView();
-		mav.setViewName("search/mapSearch");
-		return mav;
-	}
-	
-	@GetMapping("/locationSearch")
-	public ModelAndView locationSearch() throws Exception {
-		ModelAndView mav = new ModelAndView();
-		List<String[]> sidoList = spotService.getSidoList();
-//		System.out.println(sidoList);
-		mav.addObject("sidos", sidoList);
-		mav.setViewName("search/locationSearch");
-		return mav;
-	}
+//	@GetMapping("/spot/map")
+//	public ResponseEntity<> mapSearch() {
+//		ModelAndView mav = new ModelAndView();
+//		mav.setViewName("search/mapSearch");
+//		return mav;
+//	} ->vue로 하는거같음...(?)
+//	
+//	@GetMapping("/locationSearch")
+//	public ModelAndView locationSearch() throws Exception {
+//		ModelAndView mav = new ModelAndView();
+//		List<String[]> sidoList = spotService.getSidoList();
+////		System.out.println(sidoList);
+//		mav.addObject("sidos", sidoList);
+//		mav.setViewName("search/locationSearch");
+//		return mav;
+//	}
 	
 	@GetMapping("/list/{sidoid}/{gugunid}/{pageno}")
 	public ModelAndView list(@PathVariable("sidoid") int sido, @PathVariable("gugunid") int gugun, @PathVariable("pageno") int pageno) throws Exception {
@@ -53,37 +55,23 @@ public class SpotController {
 		mav.addObject("spotNum", spotNum);
 		return mav;
 	}
-	
-	@GetMapping("/gugunList/{sidoid}")
-	public ModelAndView gugunList(@PathVariable("sidoid") int sidoId) throws Exception {
-		ModelAndView mav = new ModelAndView();
-		List<String[]> gugunList = spotService.getGugunList(sidoId);
-		System.out.println(gugunList);
-		mav.addObject("guguns", gugunList);
-		mav.setViewName("search/gugun");
-		System.out.println("gugunList로 이동!!");
-		return mav;
+	@GetMapping("/sido")
+	public ResponseEntity<List<String[]>> sidoList() throws Exception{
+		return new ResponseEntity<List<String[]>>(spotService.getSidoList(),HttpStatus.OK);
+	}
+	@GetMapping("/gugun")
+	public ResponseEntity<List<String[]>> gugunList(@PathVariable("sidoid") int sidoId) throws Exception{
+		return new ResponseEntity<List<String[]>>(spotService.getGugunList(sidoId),HttpStatus.OK);
 	}
 	
-	@GetMapping("/detail/{spotid}")
-	public ModelAndView detail(@PathVariable("spotid") int spotId) throws Exception {
-		ModelAndView mav = new ModelAndView();
-		Spot spot = spotService.getSpot(spotId);
-		System.out.println(spot);
-		mav.addObject("spotDetail", spot);
-		mav.setViewName("search/spotDetail");
-		return mav;
+	@GetMapping("/{spotid}")
+	public ResponseEntity<Spot> detail(@PathVariable("spotid") int spotId) throws Exception {
+		return new ResponseEntity<Spot>(spotService.getSpot(spotId),HttpStatus.OK);
 	}
 	
-	@GetMapping("/topSpotlist/{theme}/{sidoid}")
-	public ModelAndView topSpotlist(@PathVariable("theme") String theme, @PathVariable("sidoid") int sidoId) throws Exception {
-		System.out.println("topsPotList왜 안되지");
-		ModelAndView mav = new ModelAndView();
-		List<Spot> topSpotList = spotService.getHotSpotList(theme,sidoId);
-		System.out.println(topSpotList);
-		mav.setViewName("search/topSpots");
-		mav.addObject("topSpots", topSpotList);
-		return mav;
+	@GetMapping("/top/{theme}/{sidoid}")
+	public ResponseEntity<List<Spot>> topSpotlist(@PathVariable("theme") String theme, @PathVariable("sidoid") int sidoId) throws Exception {
+		return new ResponseEntity<List<Spot>>(spotService.getHotSpotList(theme, sidoId),HttpStatus.OK);
 	}
 	
 }
