8130de4aaae5b23550edcccecdd127dbbaac2e9b
tykwon97 <tykwon97@naver.com>
2022-11-16 17:52:54 +0900

    [FE]: [FEAT] #7 로그인 라우터 연결

--- FE/src/components/user/UserLogin.vue
+++ FE/src/components/user/UserLogin.vue
@@ -5,51 +5,63 @@
       <h2 class="text-center mt-5 mb-3">로그인</h2>
 
       <div class="d-flex p-3 m-3 justify-content-center align-items-center">
-        <form
-          class="text-center"
-          id="login-form"
-          method="POST"
-          style="width: 45%"
+        <input
+          type="text"
+          class="login-form-item form-control border rounded-5 p-3"
+          id="id"
+          name="id"
+          placeholder="아이디"
+        />
+        <input
+          type="password"
+          class="login-form-item form-control border rounded-5 p-3"
+          id="pwd"
+          name="pwd"
+          placeholder="비밀번호"
+        />
+        <button
+          type="button"
+          class="btn btn-solid-lg me-2 mt-3 mb-4 w-100"
+          id="btn-login"
+          @click="confirm"
         >
-          <input
-            type="text"
-            class="login-form-item form-control border rounded-5 p-3"
-            id="login-form-id"
-            name="id"
-            placeholder="아이디"
-          />
-          <input
-            type="password"
-            class="login-form-item form-control border rounded-5 p-3"
-            id="login-form-pw"
-            name="pwd"
-            placeholder="비밀번호"
-          />
-          <button
-            type="button"
-            class="btn btn-solid-lg me-2 mt-3 mb-4 w-100"
-            id="btn-login"
-          >
-            로그인
-          </button>
-          <br />
-          <div class="mt-3">
-            <a class="login-options" href="${root}/user/signUp">회원 가입 </a> |
-            <a class="login-options" href="${root}/user/Find/id">아이디 찾기</a>
-            |
-            <a class="login-options" href="${root}/user/Find/pwd"
-              >비밀번호 찾기</a
-            ><br />
-          </div>
-        </form>
+          로그인
+        </button>
+        <br />
+        <div class="mt-3">
+          <a class="login-options" @click="movePage">회원 가입 </a> |
+          <a class="login-options" href="${root}/user/Find/id">아이디 찾기</a>
+          |
+          <a class="login-options" href="${root}/user/Find/pwd">비밀번호 찾기</a
+          ><br />
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+// import http from "@/api/http";
+
 export default {
   name: "UserLogin",
+  data() {
+    return {
+      isLoginError: false,
+      user: {
+        userid: "",
+        userpwd: "",
+      },
+    };
+  },
+  methods: {
+    confirm() {
+      alert("로그인!!!");
+    },
+    movePage() {
+      this.$router.push({ name: "SignUp" });
+    },
+  },
 };
 </script>
 
