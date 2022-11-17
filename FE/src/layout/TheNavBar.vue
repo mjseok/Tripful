76c0021da56f87518450ef919c4f6598bff181bf
tykwon97 <tykwon97@naver.com>
2022-11-17 17:36:41 +0900

    [FE] : [FEAT] NavBar 정리 및 라우터 연결, 뷰부트스트랩 js 분리

--- FE/src/layout/TheNavBar.vue
+++ FE/src/layout/TheNavBar.vue
@@ -7,62 +7,65 @@
       class="nav justify-content-center"
     >
       <b-navbar-brand href="#">
-        <router-link to="/"> </router-link>
+        <router-link to="/">Tripful</router-link>
       </b-navbar-brand>
 
-      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+      <b-navbar-toggle target="nav-collapse">햄버거</b-navbar-toggle>
 
-      <b-navbar-nav>
-        <b-nav-item href="#">
-          <router-link :to="{ name: 'index' }" class="link">
-            <b-icon icon="house" font-scale="1.5"></b-icon> 홈
-          </router-link>
-        </b-nav-item>
-        <b-nav-item href="#"
-          ><router-link :to="{ name: 'schedule' }" class="link"
-            ><b-icon icon="house" font-scale="1.5"></b-icon> 스케줄</router-link
-          ></b-nav-item
-        >
-        <b-nav-item-dropdown right>
-          <template #button-content>
-            <b-icon icon="people" font-scale="2"></b-icon>
-          </template>
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item href="#">
+            <router-link :to="{ name: 'index' }" class="link">홈</router-link>
+          </b-nav-item>
+
+          <b-nav-item-dropdown right>
+            <template #button-content> 관광지 조회 </template>
+            <b-nav-item href="#">
+              <router-link :to="{ name: 'spot' }" class="link"
+                >지역별조회</router-link
+              >
+            </b-nav-item>
+            <b-nav-item href="#"
+              ><router-link :to="{ name: 'map' }" class="link">
+                지도로 조회</router-link
+              ></b-nav-item
+            >
+          </b-nav-item-dropdown>
+
           <b-nav-item href="#"
-            ><router-link :to="{ name: 'spot' }" class="link"
-              ><b-icon icon="calendar-range" font-scale="1.5"></b-icon> 지역별
-              조회</router-link
+            ><router-link :to="{ name: 'schedule' }" class="link"
+              >스케줄</router-link
             ></b-nav-item
           >
-          <b-nav-item href="#"
-            ><router-link :to="{ name: 'map' }" class="link"
-              ><b-icon icon="pin-map-fill" font-scale="1.5"></b-icon> 지도로
-              조회</router-link
-            ></b-nav-item
-          >
-        </b-nav-item-dropdown>
 
-        <!-- <b-nav-item href="#"
-          ><router-link :to="{ name: 'house' }" class="link"
-            ><b-icon icon="house-fill" font-scale="1.5"></b-icon>
-            게시판</router-link
-          ></b-nav-item
-        >
-        <b-nav-item-dropdown right>
-          <template #button-content>
-            <b-icon icon="people" font-scale="2"></b-icon>
-          </template>
-          <b-dropdown-item href="#"
-            ><router-link :to="{ name: 'signUp' }" class="link"
-              ><b-icon icon="person-circle"></b-icon> 회원가입</router-link
-            ></b-dropdown-item
-          > -->
-        <b-dropdown-item href="#"
-          ><router-link :to="{ name: 'login' }" class="link"
-            ><b-icon icon="key"></b-icon> 로그인</router-link
-          ></b-dropdown-item
-        >
-        <!-- </b-nav-item-dropdown> -->
-      </b-navbar-nav>
+          <b-nav-item-dropdown right>
+            <template #button-content> 커뮤니티 </template>
+            <b-nav-item href="#"
+              ><router-link :to="{ name: 'test1' }" class="link"
+                >test1
+              </router-link></b-nav-item
+            >
+            <b-nav-item href="#"
+              ><router-link :to="{ name: 'test2' }" class="link"
+                >test2 조회</router-link
+              ></b-nav-item
+            >
+          </b-nav-item-dropdown>
+
+          <b-nav-item href="#">
+            <router-link :to="{ name: 'mypage' }" class="link">
+              마이페이지
+            </router-link>
+          </b-nav-item>
+
+          <b-nav-item href="#">
+            <!-- v-if="userInfo" -->
+            <router-link :to="{ name: 'login' }" class="link">
+              로그인/로그아웃
+            </router-link>
+          </b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
     </b-navbar>
   </div>
 </template>
