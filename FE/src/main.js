bd761325326ec1de60bff30d15469e1312ae35e0
mjseok <mjseok98@gmail.com>
2022-11-21 05:44:29 +0900

    [FE] : [FEAT] fontawesome 설정

--- FE/src/main.js
+++ FE/src/main.js
@@ -9,6 +9,16 @@ import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 // Import Bootstrap and BootstrapVue CSS files (order is important)
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { far } from "@fortawesome/free-regular-svg-icons";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+
+library.add(fas, far, fab);
+Vue.component("font-awesome-icon", FontAwesomeIcon);
+
 // Import kakao map ApI
 // import `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.VUE_APP_KAKAOAPI}&libraries=services`;
 // Make BootstrapVue available throughout your project
