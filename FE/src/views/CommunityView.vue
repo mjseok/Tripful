9f6020758ad74e7a5e9adbf7fa1ee3560fba068a
mjseok <mjseok98@gmail.com>
2022-11-22 06:04:21 +0900

    [FE] : [FEAT] Community,Notice 페이지 init

---
+++ FE/src/views/CommunityView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>Community
+    <boad-list title="Community"></boad-list>
+  </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ FE/src/views/NoticeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>Notice
+    <boad-list title="Notice"></boad-list>
+  </div>
+</template>
+
+<script>
+import BoardList from "@/components/board/BoardList";
+
+export default {
+    
+}
+</script>
+
+<style>
+
+</style>
