4ec6206d3b3f0f961c2227bbbc2cd4f113529f87
tykwon97 <tykwon97@naver.com>
2022-11-22 02:27:17 +0900

    [BE] : [FEAT] #18 회원탈퇴 구현 중...(Delete 메소드 사용x)

--- BE/src/main/java/com/ssafy/enjoytrip/user/controller/UserController.java
+++ BE/src/main/java/com/ssafy/enjoytrip/user/controller/UserController.java
@@ -9,11 +9,13 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -221,6 +223,19 @@ public class UserController {
 		return mav;
 	}
 
-
+	@PostMapping("/resign")
+	public ResponseEntity<?> removeUser(@RequestBody User user) {
+		
+		System.out.println("들어온다잉 "+user.getId());
+		try {
+			int uid = user.getUid();
+			userService.removeUser(uid);
+			return new ResponseEntity<User>(user,HttpStatus.OK);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
 
 }
