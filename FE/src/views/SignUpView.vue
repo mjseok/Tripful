76a96995548f2667ceae5462c6ed08e69df2308a
mjseok98 <mjseok98@gmail.com>
2022-11-21 16:12:57 +0900

    [FE] : [FEAT] #10 회원가입 화면 구현

--- FE/src/views/SignUpView.vue
+++ FE/src/views/SignUpView.vue
@@ -1,57 +1,62 @@
 <template>
-  <div >
-  <div class="container pt-lg-md mt-5">
+  <div>
+    <div class="container pt-lg-md mt-5">
       <div class="row justify-content-center">
-          <div class="col-lg-5">
-                  <app-card shadow
-                  type="light"
-                      header-classes="bg-white pb-5"
-                      body-classes="px-lg-5 py-lg-5"
-                      class="border-0">
-                  <template>
-                      <div class="text-center text-muted mb-4">
-                          Sign Up
-                      </div>
-                      <form role="form">
-                           <app-input alternative
-                                      class="mb-3"
-                                      placeholder="Id"
-                                      addonLeftIcon="person-badge-fill"
-                                      v-model="user.id"
-                                      >
-                          </app-input>
-                          <app-input alternative
-                                      type="password"
-                                      placeholder="Password"
-                                      addonLeftIcon="key-fill"
-                                      v-model="user.pwd"
-                                      >
-                          </app-input>
-                          <app-input alternative
-                                      class="mb-3"
-                                      placeholder="Name"
-                                      addonLeftIcon="person-fill"
-                                      v-model="user.name"
-                                      >
-                          </app-input>
-                          <app-input alternative
-                                      class="mb-3"
-                                      placeholder="Email"
-                                      addonLeftIcon="envelope"
-                                      v-model="user.email"
-                                      >
-                          </app-input>
-                          <div class="text-center">
-                              <app-button type="primary" class="my-4" @click="signUp">Sign Up</app-button>
-                          </div>
-                      </form>
-                  </template>
-              </app-card>
-              
-          </div>
+        <div class="col-lg-5">
+          <app-card
+            shadow
+            type="light"
+            header-classes="bg-white pb-5"
+            body-classes="px-lg-5 py-lg-5"
+            class="border-0"
+          >
+            <template>
+              <div class="text-center text-muted mb-4">Sign Up</div>
+              <form role="form">
+                <app-input
+                  alternative
+                  class="mb-3"
+                  placeholder="Id"
+                  addonLeftIcon="person-badge-fill"
+                  v-model="user.id"
+                >
+                </app-input>
+                <app-input
+                  alternative
+                  type="password"
+                  placeholder="Password"
+                  addonLeftIcon="key-fill"
+                  v-model="user.pwd"
+                >
+                </app-input>
+                <app-input
+                  alternative
+                  class="mb-3"
+                  placeholder="Name"
+                  addonLeftIcon="person-fill"
+                  v-model="user.name"
+                >
+                </app-input>
+                <app-input
+                  alternative
+                  class="mb-3"
+                  placeholder="Email"
+                  addonLeftIcon="envelope"
+                  v-model="user.email"
+                >
+                </app-input>
+                <div class="text-center">
+                  <app-button type="primary" class="my-4" @click="signUp"
+                    >Sign Up</app-button
+                  >
+                </div>
+              </form>
+            </template>
+          </app-card>
+        </div>
       </div>
+    </div>
   </div>
-</div>
 </template>
 
 <script>
@@ -63,29 +68,28 @@ import AppCard from "@/components/common/AppCard";
 const userStore = "userStore";
 export default {
   name: "SignUpView",
-  components: { AppInput,AppButton,AppCard },
+  components: { AppInput, AppButton, AppCard },
   data() {
     return {
       user: {
-        name:null,
+        name: null,
         id: null,
         pwd: null,
-        email:null,
+        email: null,
       },
     };
   },
   methods: {
-    ...mapActions(userStore,["userSignUp"]),
-      async signUp() {
-        await this.userSignUp(this.user);
-      },
+    ...mapActions(userStore, ["userSignUp"]),
+    async signUp() {
+      await this.userSignUp(this.user);
+    },
   },
 };
 // library.add();
 </script>
 
 <style lang="scss">
-@import "@/assets/styles/User/_card.scss"; 
-@import "@/assets/styles/User/_button.scss"; 
-
+@import "@/assets/styles/User/_card.scss";
+@import "@/assets/styles/User/_button.scss";
 </style>
