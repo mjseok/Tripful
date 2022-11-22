8a2cbc37d363c450f84589d833dabc45b124fa43
mjseok98 <mjseok98@gmail.com>
2022-11-22 17:02:30 +0900

    [FE]:[FEAT] #17 보드 화면

---
+++ FE/src/views/BoardView.vue
@@ -0,0 +1,20 @@
+<template>
+  <b-container class="bv-example-row mt-3 text-center">
+    <router-view></router-view>
+  </b-container>
+</template>
+<script>
+export default {
+  name: "BoardView",
+};
+</script>
+<style scoped>
+.underline-hotpink {
+  display: inline-block;
+  background: linear-gradient(
+    180deg,
+    rgba(255, 255, 255, 0) 70%,
+    rgba(231, 27, 139, 0.3) 30%
+  );
+}
+</style>
