2bad58cef5058d847d4b931490f153f191351ed7
mjseok98 <mjseok98@gmail.com>
2022-11-22 17:08:06 +0900

    [BE]:[FEAT] #17 board service 추가

--- BE/src/main/java/com/ssafy/enjoytrip/board/service/BoardServiceImpl.java
+++ BE/src/main/java/com/ssafy/enjoytrip/board/service/BoardServiceImpl.java
@@ -1,99 +1,98 @@
 package com.ssafy.enjoytrip.board.service;
 
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.ssafy.enjoytrip.board.Board;
+import com.ssafy.enjoytrip.board.mapper.BoardMapper;
 
 @Service
 public class BoardServiceImpl implements BoardService {
 
-//	private static BoardService boardService = new BoardServiceImpl();
-//	private BoardDao boardDao;
-//
-//	private BoardServiceImpl() {
-//		boardDao = BoardDaoImpl.getBoardDao();
-//	}
-//
-//	public static BoardService getBoardService() {
-//		return boardService;
-//	}
-//
-//	@Override
-//	public List<Board> listBoard(Map<String, String> map) throws Exception {
-//		int pgno = Integer.parseInt(map.get("pgno"));
-//		int spl = 15;
-//		int start = (pgno - 1) * spl;
-//		map.put("start", start + "");
-//		map.put("spl", spl + "");
-//		return boardDao.listBoard(map);
-//	}
-//
+	@Autowired
+	private BoardMapper boardMapper;
+	
+	@Override
+	public List<Board> listBoard(Map<String, Integer> map) throws Exception {
+		int pgno = map.get("pgno");
+		int spl = 15;
+		int start = (pgno - 1) * spl;
+		map.put("start", start);
+		map.put("spl", spl);
+		return boardMapper.listBoard(map);
+	}
+
 //	@Override
 //	public void writeBoard(Board board) throws Exception {
-//		boardDao.writeBoard(board);
+//		boardMapper.writeBoard(board);
 //	}
 //
 //	@Override
 //	public Board getBoard(int boardid) throws Exception {
-//		return boardDao.getBoard(boardid);
+//		return boardMapper.getBoard(boardid);
 //	}
 //
 //	@Override
 //	public void updateBoardHit(int boardid) throws Exception {
-//		boardDao.updateBoardHit(boardid);
+//		boardMapper.updateBoardHit(boardid);
 //	}
 //
 //	@Override
 //	public void modifyBoard(Board board) throws Exception {
-//		boardDao.modifyBoard(board);
+//		boardMapper.modifyBoard(board);
 //	}
 //
 //	@Override
 //	public void deleteBoard(int boardid) throws Exception {
-//		boardDao.deleteBoard(boardid);
+//		boardMapper.deleteBoard(boardid);
 //	}
 //
 //	@Override
 //	public int countBoard() throws Exception {
-//		return boardDao.countBoard();
+//		return boardMapper.countBoard();
 //	}
 //
 //	@Override
-//	public List<Board> listNotice(Map<String, String> map) throws Exception {
-//		int pgno = Integer.parseInt(map.get("pgno"));
+//	public List<Board> listNotice(Map<String, Integer> map) throws Exception {
+//		int pgno = map.get("pgno");
 //		int spl = 15;
 //		int start = (pgno - 1) * spl;
-//		map.put("start", start + "");
-//		map.put("spl", spl + "");
-//		return boardDao.listNotice(map);
+//		map.put("start", start);
+//		map.put("spl", spl);
+//		return boardMapper.listNotice(map);
 //	}
 //
 //	@Override
 //	public void writeNotice(Board notice) throws Exception {
-//		boardDao.writeNotice(notice);
+//		boardMapper.writeNotice(notice);
 //	}
 //
 //	@Override
 //	public Board getNotice(int noticeid) throws Exception {
-//		return boardDao.getNotice(noticeid);
+//		return boardMapper.getNotice(noticeid);
 //	}
 //
 //	@Override
 //	public void updateNoticeHit(int noticeid) throws Exception {
-//		boardDao.updateNoticeHit(noticeid);
+//		boardMapper.updateNoticeHit(noticeid);
 //	}
 //
 //	@Override
 //	public void modifyNotice(Board notice) throws Exception {
-//		boardDao.modifyNotice(notice);
+//		boardMapper.modifyNotice(notice);
 //	}
 //
 //	@Override
 //	public void deleteNotice(int noticeid) throws Exception {
-//		boardDao.deleteNotice(noticeid);
+//		boardMapper.deleteNotice(noticeid);
 //	}
 //
 //	@Override
 //	public int countNotice() throws Exception {
-//		return boardDao.countNotice();
+//		return boardMapper.countNotice();
 //	}
 
 }
