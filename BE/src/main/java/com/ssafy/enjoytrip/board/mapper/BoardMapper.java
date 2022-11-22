b5ed4a3bfc6896205c3052c6521d0832c83e2fee
mjseok98 <mjseok98@gmail.com>
2022-11-22 17:07:52 +0900

    [BE]:[FEAT] #17 board mapper 추가

--- BE/src/main/java/com/ssafy/enjoytrip/board/mapper/BoardMapper.java
+++ BE/src/main/java/com/ssafy/enjoytrip/board/mapper/BoardMapper.java
@@ -1,6 +1,41 @@
 package com.ssafy.enjoytrip.board.mapper;
 
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.ssafy.enjoytrip.board.Board;
+
+@Mapper
 public interface BoardMapper {
-	//보드 가져오기
+	List<Board> listBoard(Map<String, Integer> map) throws SQLException;
+
+//	void writeBoard(Board board) throws SQLException;
+//
+//	Board getBoard(int boardid) throws SQLException;
+//
+//	void updateBoardHit(int boardid) throws SQLException;
+//
+//	void modifyBoard(Board board) throws SQLException;
+//
+//	void deleteBoard(int boardid) throws SQLException;
+//
+//	int countBoard() throws SQLException;
+//	
+//	List<Board> listNotice(Map<String, Integer> map) throws SQLException;
+//
+//	void writeNotice(Board notice) throws SQLException;
+//
+//	Board getNotice(int noticeid) throws SQLException;
+//
+//	void updateNoticeHit(int noticeid) throws SQLException;
+//
+//	void modifyNotice(Board notice) throws SQLException;
+//
+//	void deleteNotice(int noticeid) throws SQLException;
+//
+//	int countNotice() throws SQLException;
 	
 }
