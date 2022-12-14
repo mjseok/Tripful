8aa62ea4d04c2c2ee2d083d5cc1c2c1112175978
tykwon97 <tykwon97@naver.com>
2022-12-14 23:38:01 +0900

    [FE]: [FEAT] #36 공지사항도 보이게 수정

--- FE/src/layout/TheNavBar.vue
+++ FE/src/layout/TheNavBar.vue
@@ -1,18 +1,9 @@
 <template>
   <div>
-    <b-navbar
-      toggleable="lg"
-      type="transparent"
-      variant="transparent"
-      class="nav"
-    >
+    <b-navbar toggleable="lg" type="transparent" variant="transparent" class="nav">
       <b-navbar-brand id="nav" href="#">
         <router-link to="/"
-          ><img
-            src="@/assets/logo2.png"
-            alt=""
-            width="25%"
-            style="text-align: 'left'"
+          ><img src="@/assets/logo2.png" alt="" width="25%" style="text-align: 'left'"
         /></router-link>
         <!-- <router-link to="/">Tripful</router-link> -->
       </b-navbar-brand>
@@ -27,9 +18,7 @@
           <b-nav-item-dropdown right>
             <template #button-content> 관광지 조회 </template>
             <b-nav-item href="#">
-              <router-link :to="{ name: 'spot' }" class="link"
-                >지역 별 조회</router-link
-              >
+              <router-link :to="{ name: 'spot' }" class="link">지역 별 조회</router-link>
             </b-nav-item>
             <b-nav-item href="#"
               ><router-link :to="{ name: 'map' }" class="link">
@@ -39,12 +28,9 @@
           </b-nav-item-dropdown>
 
           <b-nav-item href="#"
-            ><router-link :to="{ name: 'schedule' }" class="link"
-              >스케줄</router-link
-            ></b-nav-item
+            ><router-link :to="{ name: 'schedule' }" class="link">스케줄</router-link></b-nav-item
           >
-
-          <!-- <b-nav-item-dropdown right>
+          <b-nav-item-dropdown right>
             <template #button-content> 커뮤니티 </template>
             <b-nav-item href="#"
               ><router-link :to="{ name: 'community' }" class="link"
@@ -52,18 +38,12 @@
               </router-link></b-nav-item
             >
             <b-nav-item href="#"
-              ><router-link :to="{ name: 'notice' }" class="link"
-                >공지사항</router-link
-              ></b-nav-item
+              ><router-link :to="{ name: 'notice' }" class="link">공지사항</router-link></b-nav-item
             >
-          </b-nav-item-dropdown> -->
-          <b-nav-item href="#"
-            ><router-link :to="{ name: 'community' }" class="link"
-              >커뮤니티
-            </router-link></b-nav-item
-          >
+          </b-nav-item-dropdown>
+
           <!-- after login -->
-  
+
           <b-nav-item class="align-self-center" v-if="userInfo">
             {{ userInfo.name }}({{ userInfo.id }})님 환영합니다.
           </b-nav-item>
@@ -82,18 +62,13 @@
             </b-nav-item>
           </b-nav-item-dropdown>
 
-          <b-nav-item
-            class="align-self-center link"
-            @click.prevent="onClickLogout"
-            v-if="userInfo"
+          <b-nav-item class="align-self-center link" @click.prevent="onClickLogout" v-if="userInfo"
             >로그아웃</b-nav-item
           >
           <!-- before login -->
 
           <b-nav-item href="#" v-else>
-            <router-link :to="{ name: 'login' }" class="link">
-              로그인</router-link
-            >
+            <router-link :to="{ name: 'login' }" class="link"> 로그인</router-link>
           </b-nav-item>
           <!-- </b-nav-item-dropdown> -->
         </b-navbar-nav>
